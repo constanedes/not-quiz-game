91e460c60cc437ff6e5752cb989236deffbb21e8
Constantino Edes <constantino.edes@gmail.com>
2023-05-28 03:24:20 -0300

    Update dependencies

--- src/utils/dedent.ts
+++ src/utils/dedent.ts
@@ -6,9 +6,7 @@ export default function dedent(callSite: TemplateStringsArray, ...args: any[]): 
             return "\n" + m1.slice(Math.min(m1.length, size));
         });
     }
-
     if (typeof callSite === "string") return format(callSite);
-
     const output = callSite
         .slice(0, args.length + 1)
         .map((text: string, i: number) => (i === 0 ? "" : args[i - 1]) + text)
