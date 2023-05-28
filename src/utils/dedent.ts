d73d560e3056712d778a06c7595b4371f68e8039
Constantino Edes <constantino.edes@gmail.com>
2023-05-27 21:42:46 -0300

    Add new features and extra files

---
+++ src/utils/dedent.ts
@@ -0,0 +1,18 @@
+export default function dedent(callSite: TemplateStringsArray, ...args: any[]): string | ((...args: any[]) => string) {
+    function format(str: string): string {
+        let size = -1;
+        return str.replace(/\n(\s+)/g, (m: string, m1: string) => {
+            if (size < 0) size = m1.replace(/\t/g, "    ").length;
+            return "\n" + m1.slice(Math.min(m1.length, size));
+        });
+    }
+
+    if (typeof callSite === "string") return format(callSite);
+
+    const output = callSite
+        .slice(0, args.length + 1)
+        .map((text: string, i: number) => (i === 0 ? "" : args[i - 1]) + text)
+        .join("");
+
+    return format(output);
+}
