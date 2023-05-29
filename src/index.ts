03aac7088a01ac9ff35d342988dafa56ccd45423
Constantino Edes <constantino.edes@gmail.com>
2023-05-29 00:33:39 -0300

    Create game core WIP

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,20 @@
 #!/usr/bin/env node
-import { setTimeout } from 'timers/promises';
-import fs from "fs/promises"
 import { join } from "path";
 import chalk from "chalk";
 import dedent from "./utils/dedent";
 import inquirer from "inquirer";
-import LoggerService from './utils/logger';
+import IQuestion from "./interfaces/IQuestion";
+import LoggerService from "./services/loggerService";
+import FileStorageService from "./services/fileStorageService";
+import getQuestions from "./helpers/questions";
+import { showMenuBanner } from "./helpers/menu";
 
+// global variables
 const questionsFilePath: string = join(__dirname, "questions.json");
 const lives: number = 3;
 
-async function sleep(ms: number) {
-    await setTimeout(ms)
-} 
-
-interface IQuestion {
-    title: string;
-    correct: number;
-    options: string[];
-}
-
 function getRandomNumber(min: number, max: number) {
     return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-async function getQuestions() {
-    const allQuestionsObject = JSON.parse(await fs.readFile(questionsFilePath, "utf-8")) as IQuestion[];
-    console.log(allQuestionsObject);
-    return new Map<number, IQuestion>(allQuestionsObject.map((e: IQuestion, index: number) => [index, e]));
 }
 
 const data = [
@@ -54,16 +41,13 @@ async function getRandomQuestion(): Promise<IQuestion | undefined> {
     if (lives === 0) {
         process.exit(1);
     }
-    const data = await getQuestions();
+    const data = await getQuestions(questionsFilePath);
     const randomIndex = getRandomNumber(0, data.size);
     return data.get(randomIndex);
 }
 
-async function welcome() {
-    /* const rainbowTitle = chalkAnimation.rainbow(
-		"Welcome to the best quiz game! \n",
-	);
-	rainbowTitle.stop(); */
+function welcome() {
+    showMenuBanner();
 
     console.log(dedent`
 		${chalk.bgBlack.underline.whiteBright("HOW TO PLAY")} 
@@ -101,15 +85,13 @@ async function handleAnswer(question: IQuestion, userAnswer: string) {
         text: "Checking answer",
     }).start();
     */
-  
-
     /* spinner.stop(); */
 
     if (question["options"][question.correct] === userAnswer) {
         //spinner.succeed("Correct");
-        console.log("correct")
-        console.log(userAnswer)
-        
+        console.log("correct");
+        console.log(userAnswer);
+
         //spinner.info(`You got ${lives} lives, and ${} of ${} questions`)
     } else {
         //spinner.fail("Incorrect...");
@@ -118,16 +100,12 @@ async function handleAnswer(question: IQuestion, userAnswer: string) {
 }
 
 async function main() {
-    await sleep(100);
     welcome();
-    console.log(questionsFilePath);
-    const nextQuestion = await getRandomQuestion()
-    if(nextQuestion){
+    /* console.log(questionsFilePath);
+    const nextQuestion = await getRandomQuestion();
+    if (nextQuestion) {
         viewNextQuestionOptions(nextQuestion);
-    }  
-
-
- 
+    } */
 }
 
 main();
