2c8a94e8ac886bc9d2927b5149d22555f1ebcb44
Constantino Edes <constantino.edes@gmail.com>
2023-06-18 19:24:13 -0300

    Major file refactor, move helpers to services

--- src/utils/handleError.ts
+++
@@ -1,9 +0,0 @@
-import LoggerService from "../services/loggerService";
-
-export default function handleError(logger: LoggerService, error: unknown): void {
-    if (error instanceof Error) {
-        logger.error(error.message);
-    } else {
-        logger.error(`Unknown error: ${error}`);
-    }
-}
