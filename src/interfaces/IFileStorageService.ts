3f0aba827bc6c7d2bc4bd8357ca731094251b991
Constantino Edes <constantino.edes@gmail.com>
2023-07-22 18:04:59 -0300

    Remove unneccessary interfaces

--- src/interfaces/IFileStorageService.ts
+++
@@ -1,8 +0,0 @@
-import { JsonObject } from "type-fest";
-
-export interface IFileStorageService {
-    readJsonFile(filePath: string, parse: boolean): string | unknown;
-    createFile(filePath: string): void;
-    checkFileExists(filepath: string): boolean;
-    addObjectToJson(filePath: string, jsonData: JsonObject): void;
-}
--- src/interfaces/ILoggerService.ts
+++
@@ -1,8 +0,0 @@
-export interface ILoggerService {
-    trace(message: string, optionalParams?: unknown[]): void;
-    log(message: string, optionalParams?: unknown[]): void;
-    debug(message: string, optionalParams?: unknown[]): void;
-    info(message: string, optionalParams?: unknown[]): void;
-    warning(message: string, optionalParams?: unknown[]): void;
-    error(message: string, optionalParams?: unknown[]): void;
-}
